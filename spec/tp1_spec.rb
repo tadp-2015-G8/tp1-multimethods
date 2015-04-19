520e5cf9d5b7bbffff1adab56af40ec5a346cc87
Matías Mangiantini <matiasm15@hotmail.com>
2015-04-18 23:57:39 -0300

    Agregado soporte inicial para multimetodos.
    Se puede usar self y atributos de instancia dentro de los bloques. Hice
    hasta lo pedido en la pagina 4, el cálculo de distancia de parámetros.

--- spec/tp1_spec.rb
+++ spec/tp1_spec.rb
@@ -42,7 +42,50 @@ describe 'test PartialBlock' do
       3.14159265359
     end
 
-    expect(pi.call()).to eq( 3.14159265359)
+    expect(pi.call()).to eq(3.14159265359)
     expect(pi.matches()).to eq(true)
+  end
+
+  it 'Multimetodos' do
+    class A
+      partial_def :concat, [Object, Object] do |o1, o2|
+        "Objetos concatenados"
+      end
+
+      partial_def :concat, [String, String] do |s1, s2|
+        s1 + s2
+      end
+
+      partial_def :concat, [String, Integer] do |s1, n|
+        s1 * n
+      end
+
+      partial_def :concat, [Array] do |a|
+        a.join
+      end
+
+      partial_def :clase, [] do
+        self.class
+      end
+
+      partial_def :var, [] do
+        @var
+      end
+
+      partial_def :var=, [Integer] do |arg|
+        @var = arg
+      end
+    end
+
+    expect(A.new.concat("hello", " world")).to eq("hello world")
+    expect(A.new.concat("hello", 3)).to eq("hellohellohello")
+    expect(A.new.concat(["hello", " world", "!"])).to eq("hello world!")
+    expect(A.new.concat(Object.new, 3)).to eq("Objetos concatenados")
+    expect(A.new.clase).to eq(A)
+    expect {A.new.concat("hello", " world", "!")}.to raise_error(ArgumentError)
+
+    b = A.new
+    b.var = 123
+    expect(b.var).to eq(123)
   end
 end
--- src/partial_block.rb
+++ src/partial_block.rb
@@ -1,4 +1,7 @@
+
 class PartialBlock
+  attr_reader :tipos
+
   def initialize(tipos, &block)
     raise ArgumentError, "Cantidad de parametros invalida" if tipos and tipos.length != block.arity
     @tipos = tipos
@@ -15,12 +18,48 @@ class PartialBlock
     return true
   end
 
-  def call(*args)
-    if !self.matches(*args)
+ def call_with_object(object, *args)
+    unless matches(*args)
       raise ArgumentError, "Argumentos invalidos"
     end
 
-    @block.call(*args)
+    object.instance_exec(*args, &@block)
+ end
+
+  def call(*args)
+    call_with_object(self, *args)
   end
 end
 
+class Class
+  def multimethods
+    @multimethods ||= Hash.new
+  end
+
+  private
+
+  def partial_def(nuevo_metodo, tipos, &bloque)
+    multimethods[nuevo_metodo] ||= Array.new
+    multimethods[nuevo_metodo] << PartialBlock.new(tipos, &bloque)
+
+    unless method_defined?(nuevo_metodo)
+      define_method(nuevo_metodo) do |*argumentos|
+        candidatos = self.class.multimethods[nuevo_metodo].select do |partial_block|
+          partial_block.matches(*argumentos)
+        end
+
+        if candidatos.empty?
+          raise ArgumentError, "#{nuevo_metodo} no esta definido para los argumentos recibidos"
+        end
+
+        candidatos.min_by { |partial_block|
+
+          argumentos.zip(partial_block.tipos, 1..(argumentos.size + 1)).map { |argumento, tipo, indice|
+            indice * argumento.class.ancestors.index(tipo)
+          }.inject(:+)
+
+        }.call_with_object(self, *argumentos)
+      end
+    end
+  end
+end
