f31068395515cb877c704318c3ed277b5a675470
Lionel Yoshida <liozzs@gmail.com>
2015-04-29 19:57:07 -0300

    *  Un multimethod que pisa un método común por lo tanto está bien que se habiliten los multimethods: Vuelta atras del ultimo cambio que borraba metodos parciales y tambien se arreglo el test de Matias para reflejar este cambio.

--- src/multimethods.rb
+++ src/multimethods.rb
@@ -2,10 +2,6 @@ require_relative '../src/partial_block'
 
 module Multimethods
 
-  def initialize
-    @@is_partial_method = false
-  end
-
   # variable que guarda los blocks de cada multimethod.
   def partial_blocks
     @partial_blocks ||= {}
@@ -79,14 +75,6 @@ end
 
 class Object
   include Multimethods
-
-  #Borra los multimetodos definidos antes de una definicion normal en una misma clase
-  def self.method_added(method_name)
-    @@is_partial_method ||= false
-    if not @@is_partial_method and not multimethods.empty?
-      partial_blocks.delete(method_name)
-    end
-  end
 end
 
 
